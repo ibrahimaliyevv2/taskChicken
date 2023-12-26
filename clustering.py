1bcc7c9208d53146bd3222a49129cd258abf28de
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2023-12-26 13:18:19 +0400

    changes

--- clustering.py
+++ clustering.py
@@ -1,55 +1,58 @@
+import io
 import os
-import matplotlib.pyplot as plt
-from PIL import Image
 import numpy as np
+from PIL import Image
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 import torch
 import clip
 
+app = FastAPI()
+
 # Load the OpenAI CLIP model with only the image encoder
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device)
 model.visual.eval()
 
-# Replace these paths with the paths to your own images
-image_paths = ["./content/photoimagenew.jpg", "./content/photonext.jpg"]
+# Placeholder for image embeddings and KMeans model
+image_embeddings = None
+kmeans_model = None
 
-image_embeddings = []
 
-for image_path in image_paths:
-    image = Image.open(image_path).convert("RGB")
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
     image_input = preprocess(image).unsqueeze(0).to(device)
-
-    with torch.no_grad():  # Corrected line
+    with torch.no_grad():
         image_embedding = model.encode_image(image_input)
+    return image_embedding.cpu().numpy().flatten()
 
-    # Convert the image embedding to a 1D numpy array
-    image_embedding = image_embedding.cpu().numpy().flatten()
 
-    image_embeddings.append(image_embedding)
+@app.post("/cluster_images/")
+async def cluster_images(files: UploadFile = File(...)):
+    global image_embeddings, kmeans_model
 
-# Convert embeddings to a 2D numpy array
-image_embeddings = np.array(image_embeddings)
+    # Process uploaded images and update image_embeddings
+    images = await files.read()
+    image_embedding = preprocess_image(images)
 
-# Apply PCA to reduce dimensionality to 2
-pca = PCA(n_components=2)
-image_embeddings_2d = pca.fit_transform(image_embeddings)
+    if image_embeddings is None:
+        image_embeddings = image_embedding.reshape(1, -1)
+    else:
+        image_embeddings = np.vstack([image_embeddings, image_embedding])
 
-# Apply KMeans clustering on the 2D embeddings
-num_clusters = 2  # Adjust as needed
-kmeans = KMeans(n_clusters=num_clusters, n_init=10)
-cluster_assignments = kmeans.fit_predict(image_embeddings_2d)
+    # Check if clustering needs to be updated
+    if kmeans_model is None or image_embeddings.shape[0] > kmeans_model.n_clusters:
+        num_clusters = image_embeddings.shape[0]  # Adjust as needed
+        kmeans_model = KMeans(n_clusters=num_clusters, n_init=10)
+        cluster_assignments = kmeans_model.fit_predict(image_embeddings)
 
-# Visualize the clusters in 2D
-plt.figure(figsize=(8, 8))
-for cluster_id in range(num_clusters):
-    cluster_indices = np.where(cluster_assignments == cluster_id)[0]
-    plt.scatter(image_embeddings_2d[cluster_indices, 0], image_embeddings_2d[cluster_indices, 1], label=f"Cluster {cluster_id + 1}")
+    return JSONResponse(content={"cluster_assignments": cluster_assignments.tolist()})
 
-plt.title("Image Clustering Visualization using PCA")
-plt.xlabel("Principal Component 1")
-plt.ylabel("Principal Component 2")
-plt.legend()
-plt.show()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- for_video.py
+++ for_video.py
@@ -1,10 +1,97 @@
+# import os
+# from PIL import Image
+# import numpy as np
+# from sklearn.cluster import KMeans
+# from sklearn.decomposition import PCA
+# import cv2
+
+
+# import torch
+# import clip
+
+# # Load the OpenAI CLIP model with only the image encoder
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model, preprocess = clip.load("ViT-B/32", device)
+# model.visual.eval()
+
+# # Replace these with the paths to your video files
+# video_paths = ["./videos/10 Second Video Loop.mp4", "./videos/10 Seconds Of Chickens.mp4"]
+
+# # Parameters for clustering and PCA
+# num_clusters = 2  # Adjust as needed
+# pca_components = 2
+
+# # Initialize a list to store video embeddings
+# video_embeddings = []
+
+# # Process each video and obtain video embeddings
+# for video_path in video_paths:
+#     print(f"Processing video: {video_path}")
+
+#     # Capture the video
+#     cap = cv2.VideoCapture(video_path)
+#     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+#     # Initialize a list to store frame embeddings
+#     frame_embeddings = []
+
+#     # Process each frame and obtain image embeddings
+#     for _ in range(frame_count):
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         # Convert the frame to RGB and preprocess for the CLIP model
+#         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+#         # Convert NumPy array to PIL image
+#         image_pil = Image.fromarray(image)
+
+#         image_input = preprocess(image_pil).unsqueeze(0).to(device)
+
+#         # Obtain image embedding using the CLIP model
+#         with torch.no_grad():
+#             image_embedding = model.encode_image(image_input)
+
+#         frame_embeddings.append(image_embedding.cpu().numpy()[0])  # Use [0] to get the actual embedding
+
+#     cap.release()
+
+#     # Calculate the average embedding for the video
+#     average_video_embedding = np.mean(frame_embeddings, axis=0)
+#     video_embeddings.append(average_video_embedding)
+
+# # Convert embeddings to numpy array
+# video_embeddings = np.array(video_embeddings)
+
+# # Apply KMeans clustering on the video embeddings
+# kmeans = KMeans(n_clusters=num_clusters, n_init=10)
+# video_cluster_assignments = kmeans.fit_predict(video_embeddings)
+
+# # Apply PCA to reduce dimensionality of cluster centers to the specified number of components
+# pca = PCA(n_components=pca_components)
+# cluster_centers_2d = pca.fit_transform(kmeans.cluster_centers_)
+
+# # Visualize the cluster centers in 2D
+# import matplotlib.pyplot as plt
+
+# plt.figure(figsize=(8, 8))
+# for cluster_id in range(num_clusters):
+#     plt.scatter(cluster_centers_2d[cluster_id, 0], cluster_centers_2d[cluster_id, 1], label=f"Cluster {cluster_id + 1}")
+
+# plt.title("Video Clustering Visualization using KMeans and PCA")
+# plt.xlabel("Principal Component 1")
+# plt.ylabel("Principal Component 2")
+# plt.legend()
+# plt.show()
+
+
 import os
 from PIL import Image
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import cv2
-
 
 import torch
 import clip
@@ -53,7 +140,8 @@ for video_path in video_paths:
         with torch.no_grad():
             image_embedding = model.encode_image(image_input)
 
-        frame_embeddings.append(image_embedding.cpu().numpy()[0])  # Use [0] to get the actual embedding
+        # Use [0] to get the actual embedding
+        frame_embeddings.append(image_embedding.cpu().numpy()[0])
 
     cap.release()
 
@@ -64,20 +152,28 @@ for video_path in video_paths:
 # Convert embeddings to numpy array
 video_embeddings = np.array(video_embeddings)
 
+# Print the number of features returned by CLIP model
+print(f"Number of features returned by CLIP model: {video_embeddings.shape[1]}")
+
 # Apply KMeans clustering on the video embeddings
 kmeans = KMeans(n_clusters=num_clusters, n_init=10)
 video_cluster_assignments = kmeans.fit_predict(video_embeddings)
 
+# Print the number of features KMeans clustering over
+print(f"Number of features KMeans clustering over: {video_embeddings.shape[1]}")
+
 # Apply PCA to reduce dimensionality of cluster centers to the specified number of components
 pca = PCA(n_components=pca_components)
-cluster_centers_2d = pca.fit_transform(kmeans.cluster_centers_)
+video_embeddings_2d = pca.fit_transform(video_embeddings)
 
 # Visualize the cluster centers in 2D
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(8, 8))
 for cluster_id in range(num_clusters):
-    plt.scatter(cluster_centers_2d[cluster_id, 0], cluster_centers_2d[cluster_id, 1], label=f"Cluster {cluster_id + 1}")
+    plt.scatter(video_embeddings_2d[video_cluster_assignments == cluster_id, 0],
+                video_embeddings_2d[video_cluster_assignments == cluster_id, 1],
+                label=f"Cluster {cluster_id + 1}")
 
 plt.title("Video Clustering Visualization using KMeans and PCA")
 plt.xlabel("Principal Component 1")
